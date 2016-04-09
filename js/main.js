d084b489b3ddebac0f0de5a2a761736b4bb23ffe
Thach Hoang <thachhoang@users.noreply.github.com>
2016-04-08 21:58:38 -0400

    Add map

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+/* globals $:false, L:false */
+(function() {
+    var map = L.map('map').setView([38.9897623, -76.9447152], 16);
+    var baseLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+}());
