afcb04a250fb31d782434f3e7bb6cd4692e21d82
Thach Hoang <thachhoang@users.noreply.github.com>
2016-04-08 23:32:55 -0400

    Add machines to the map

--- js/main.js
+++ js/main.js
@@ -5,4 +5,29 @@
         maxZoom: 18,
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
+
+    $.getJSON("data/buildings_with_machines.json", function(data) {
+        var machineLayer = L.geoJson(data, {
+            onEachFeature: function(feature, layer) {
+                var html = '<strong>' + feature.properties.name + ' (' + feature.properties.building_id + ')</strong>';
+                html += '<ul>';
+                var locations = feature.properties.locations;
+                for (var i = 0; i < locations.length; i++) {
+                    var loc = locations[i];
+                    html += '<li>' + loc.location + '<ul>';
+                    for (var k = 0; k < loc.machines.length; k++) {
+                        var m = loc.machines[k];
+                        html += '<li>' + m.name + ' (' + m.count + ')</li>';
+                    }
+                    html += '</ul></li>';
+                }
+                html += '</ul>';
+                html += '<a target="_blank" href="' + feature.properties.url + '">source</a>';
+                layer.bindPopup(html, {
+                    className: 'pop-up',
+                });
+            },
+        });
+        map.addLayer(machineLayer);
+    });
 }());
