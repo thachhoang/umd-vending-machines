564ff1143a8866898043eef88621e1f29e480219
Thach Hoang <thachhoang@users.noreply.github.com>
2016-04-15 23:20:38 -0400

    Add repo link

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
     $.getJSON("data/buildings_with_machines.json", function(data) {
         var attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
         attribution += ' | <a href="//dbs.umd.edu/corp/vending_list.php">dbs.umd.edu</a>, last fetched ' + data.generated_date;
+        attribution += ' | <a href="//github.com/thachhoang/umd-vending-machines">code</a>';
         var map = L.map('map').setView([38.9897623, -76.9447152], 16);
         var baseLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             maxZoom: 18,
